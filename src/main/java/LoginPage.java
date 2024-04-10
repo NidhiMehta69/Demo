8551b33490e7d984c75f1d3dfd1b943d163f82b7
nim@extern.csatp.com <nim@extern.csatp.com>
2024-04-10 18:58:45 +0530

    This is my first commit va command line

---
+++ src/main/java/LoginPage.java
@@ -0,0 +1,8 @@
+
+public class LoginPage {
+
+	public void Testmessage()
+	{
+		System.out.println("This is login page.");
+	}
+}
---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,8 @@
+
+public class LoginTest {
+	public void Testmessage()
+	{
+		System.out.println("This is test class.");
+	}
+
+}
